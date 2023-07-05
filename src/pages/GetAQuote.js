e36d0883c4c3d7c804259f893035688b2d832342
chad <chatmontoya32@gmail.com>
2023-07-05 16:00:44 -0600

    remove react-hooks-form

--- src/pages/GetAQuote.js
+++ src/pages/GetAQuote.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
 import '../getaquote.css';
 
 const GetAQuote = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: ''
+  });
 
-  const onSubmit = () => {
-    // The form submission will be automatically handled by Netlify.
-    // You don't need to manually handle the submission or send a fetch request.
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission
     alert('Thank you for submitting your request! We will get back to you shortly.');
   };
 
@@ -15,30 +23,26 @@ const GetAQuote = () => {
     <div className="get-a-quote">
       <h2>Get a Quote</h2>
       <p>At our window cleaning business, we charge around $9 per window, although prices may vary based on different conditions such as window size and location. For accurate pricing, we recommend scheduling an appointment to have us provide an in-person quote. To request a quote, please fill in the information below and we will get in touch with you shortly:</p>
-      <form onSubmit={handleSubmit(onSubmit)} data-netlify="true">
+      <form onSubmit={handleSubmit} data-netlify="true">
         <input type="hidden" name="form-name" value="contact" />
         <label>
           Name: <br />
-          <input type="text" name="name" {...register('name', { required: true })} />
-          {errors.name && <span className="error">Name is required</span>}
+          <input type="text" name="name" value={formData.name} onChange={handleChange} required />
         </label>
         <br />
         <label>
           Email: <br />
-          <input type="email" name="email" {...register('email', { required: true })} />
-          {errors.email && <span className="error">Email is required</span>}
+          <input type="email" name="email" value={formData.email} onChange={handleChange} required />
         </label>
         <br />
         <label>
           Phone: <br />
-          <input type="tel" name="phone" {...register('phone', { required: true })} />
-          {errors.phone && <span className="error">Phone is required</span>}
+          <input type="tel" name="phone" value={formData.phone} onChange={handleChange} required />
         </label>
         <br />
         <label>
           Message: <br />
-          <textarea name="message" {...register('message', { required: true })} />
-          {errors.message && <span className="error">Message is required</span>}
+          <textarea name="message" value={formData.message} onChange={handleChange} required></textarea>
         </label>
         <br />
         <button type="submit">Submit</button>
