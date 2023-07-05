dfb66352e0f3371f05fa829d4bb3186eeb6722d1
chad <chatmontoya32@gmail.com>
2023-07-05 15:56:53 -0600

    fix react-hooks

--- src/pages/GetAQuote.js
+++ src/pages/GetAQuote.js
@@ -5,38 +5,17 @@ import '../getaquote.css';
 const GetAQuote = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
 
-  const onSubmit = async (data) => {
-    try {
-      const response = await fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: encodeFormData(data)
-      });
-
-      if (response.ok) {
-        console.log('Form submission successful');
-        alert('Thank you for submitting your request! We will get back to you shortly.');
-      } else {
-        console.error('Form submission failed');
-        alert('Oops! Something went wrong. Please try again later.');
-      }
-    } catch (error) {
-      console.error('Form submission error:', error);
-      alert('Oops! Something went wrong. Please try again later.');
-    }
-  };
-
-  const encodeFormData = (data) => {
-    return Object.keys(data)
-      .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-      .join('&');
+  const onSubmit = () => {
+    // The form submission will be automatically handled by Netlify.
+    // You don't need to manually handle the submission or send a fetch request.
+    alert('Thank you for submitting your request! We will get back to you shortly.');
   };
 
   return (
     <div className="get-a-quote">
       <h2>Get a Quote</h2>
       <p>At our window cleaning business, we charge around $9 per window, although prices may vary based on different conditions such as window size and location. For accurate pricing, we recommend scheduling an appointment to have us provide an in-person quote. To request a quote, please fill in the information below and we will get in touch with you shortly:</p>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} data-netlify="true">
         <input type="hidden" name="form-name" value="contact" />
         <label>
           Name: <br />
