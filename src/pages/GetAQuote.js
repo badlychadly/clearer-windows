5c98662faffab935f4451c8b0ddb3ad6386b75ad
chad <chatmontoya32@gmail.com>
2023-07-11 12:54:22 -0600

    test working form with the rest of the html

--- src/pages/GetAQuote.js
+++ src/pages/GetAQuote.js
@@ -13,7 +13,7 @@ import '../getaquote.css';
 
 
 
-  const GetAQuote = () => {
+  // const GetAQuote = () => {
     // const [formData, setFormData] = useState({ name: '', email: '', message: '' });
   
     // const handleSubmit = (e) => {
@@ -33,8 +33,69 @@ import '../getaquote.css';
   
     // const { name, email, message } = formData;
   
-    return (
-      <form name="contact v2" method="post" data-netlify="true" onSubmit="submit" data-netlify-honeypot="bot-field">
+  //   return (
+  //     <form name="contact v2" method="post" data-netlify="true" onSubmit="submit" data-netlify-honeypot="bot-field">
+  //       <input type="hidden" name="form-name" value="contact v2" />
+  //       <div hidden>
+  //         <input name='bot-field'/>
+  //       </div>
+  //       <p>
+  //         <label>
+  //           Your Name: <input type="text" name="name"/>
+  //         </label>
+  //       </p>
+  //       <p>
+  //         <label>
+  //           Your Email: <input type="email" name="email"/>
+  //         </label>
+  //       </p>
+  //       <p>
+  //         <label>
+  //           Message: <textarea name="message"/>
+  //         </label>
+  //       </p>
+  //       <p>
+  //         <button type="submit">Send</button>
+  //       </p>
+  //     </form>
+  //   );
+  // };
+
+
+
+
+
+
+
+
+
+
+const GetAQuote = () => {
+  // const [formData, setFormData] = useState({
+  //   name: '',
+  //   email: '',
+  //   phone: '',
+  //   message: ''
+  // });
+
+  // const handleChange = (e) => {
+  //   setFormData({ ...formData, [e.target.name]: e.target.value });
+  // };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   // Handle form submission
+  //   alert('Thank you for submitting your request! We will get back to you shortly.');
+  // };
+
+  return (
+    <div className="get-a-quote">
+      <h2>Get a Quote</h2>
+      <p>At our window cleaning business, we charge around $9 per window, although prices may vary based on different conditions such as window size and location. For accurate pricing, we recommend scheduling an appointment to have us provide an in-person quote. To request a quote, please fill in the information below and we will get in touch with you shortly:</p>
+      
+      
+
+      {/* <form name="contact v2" method="post" data-netlify="true" onSubmit="submit" data-netlify-honeypot="bot-field">
         <input type="hidden" name="form-name" value="contact v2" />
         <div hidden>
           <input name='bot-field'/>
@@ -57,67 +118,44 @@ import '../getaquote.css';
         <p>
           <button type="submit">Send</button>
         </p>
+      </form> */}
+
+
+
+
+
+      <form name="contact v2" method="post" data-netlify="true" onSubmit="submit" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact v2" />
+        <div hidden>
+          <input name='bot-field'/>
+        </div>
+        <label>
+          Name: <br />
+          <input type="text" name="name"/>
+        </label>
+        <br />
+        <label>
+          Email: <br />
+          <input type="email" name="email"/>
+        </label>
+        <br />
+        <label>
+          Phone: <br />
+          <input type="tel" name="phone"/>
+        </label>
+        <br />
+        <label>
+          Message: <br />
+          <textarea name="message"/>
+        </label>
+        <br />
+        <button type="submit">Submit</button>
       </form>
-    );
-  };
 
 
 
-
-
-
-
-
-
-
-// const GetAQuote = () => {
-//   const [formData, setFormData] = useState({
-//     name: '',
-//     email: '',
-//     phone: '',
-//     message: ''
-//   });
-
-//   const handleChange = (e) => {
-//     setFormData({ ...formData, [e.target.name]: e.target.value });
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     // Handle form submission
-//     alert('Thank you for submitting your request! We will get back to you shortly.');
-//   };
-
-//   return (
-//     <div className="get-a-quote">
-//       <h2>Get a Quote</h2>
-//       <p>At our window cleaning business, we charge around $9 per window, although prices may vary based on different conditions such as window size and location. For accurate pricing, we recommend scheduling an appointment to have us provide an in-person quote. To request a quote, please fill in the information below and we will get in touch with you shortly:</p>
-//       <form onSubmit={handleSubmit} name="contact" netlify="true">
-//         <input type="hidden" name="form-name" value="contact" />
-//         <label>
-//           Name: <br />
-//           <input type="text" name="name" value={formData.name} onChange={handleChange} required />
-//         </label>
-//         <br />
-//         <label>
-//           Email: <br />
-//           <input type="email" name="email" value={formData.email} onChange={handleChange} required />
-//         </label>
-//         <br />
-//         <label>
-//           Phone: <br />
-//           <input type="tel" name="phone" value={formData.phone} onChange={handleChange} required />
-//         </label>
-//         <br />
-//         <label>
-//           Message: <br />
-//           <textarea name="message" value={formData.message} onChange={handleChange} required></textarea>
-//         </label>
-//         <br />
-//         <button type="submit">Submit</button>
-//       </form>
-//     </div>
-//   );
-// };
+    </div>
+  );
+};
 
 export default GetAQuote;
